c75b1702fe21d245369bffe63927b8910ac7e7e3
youmus <165415930+youmus@users.noreply.github.com>
2025-07-30 19:30:11 +0800

    Update Branding.vue

--- app/javascript/shared/components/Branding.vue
+++ app/javascript/shared/components/Branding.vue
@@ -26,18 +26,7 @@ export default {
   },
   computed: {
     brandRedirectURL() {
-      try {
-        const referrerHost = this.$store.getters['appConfig/getReferrerHost'];
-        const baseURL = `${this.globalConfig.widgetBrandURL}?utm_source=${
-          referrerHost ? 'widget_branding' : 'survey_branding'
-        }`;
-        if (referrerHost) {
-          return `${baseURL}&utm_referrer=${referrerHost}`;
-        }
-        return baseURL;
-      } catch (e) {
-        // Suppressing the error as getter is not defined in some cases
-      }
+      
       return '';
     },
   },
